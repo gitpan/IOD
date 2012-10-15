6652c0158135e75ec44541d861bafa0c4139546a
Steven Haryanto <stevenharyanto@gmail.com>
2012-10-15 15:22:39 +0000

    Import of SHARYANTO/IOD-0.9.0 from CPAN.
    gitpan-cpan-distribution: IOD
    gitpan-cpan-version:      0.9.0
    gitpan-cpan-path:         SHARYANTO/IOD-0.9.0.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/IOD.pm
@@ -0,0 +1,33 @@
+package IOD;
+
+our $VERSION = '0.9.0'; # VERSION
+
+1;
+# ABSTRACT: IOD file format specification
+
+__END__
+=pod
+
+=head1 NAME
+
+IOD - IOD file format specification
+
+=head1 VERSION
+
+version 0.9.0
+
+=head1 FUNCTIONS
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
